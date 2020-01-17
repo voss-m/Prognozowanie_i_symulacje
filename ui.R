44a6e03eae8ca92ba078205bac9c89ab6df6d4e6
voss-m <michelmaciejvoss@gmail.com>
2020-01-17 23:02:48 +0100

    Dodano kod do automatycznego instalowania pakietów

--- ui.R
+++ ui.R
@@ -10,7 +10,7 @@ ui <- fluidPage(
     sidebarPanel(
       
       # br() element to introduce extra vertical spacing ----
-      br(),
+      br()
     ),
     
     # Main panel for displaying outputs ----
@@ -25,11 +25,13 @@ ui <- fluidPage(
                            img(src='mapa_dom_mieszkanie.png', align = "center", heigt=600, width=600)
                            ),
                   tabPanel("Holt",
-                           p("Poniższy wykres przedstawia zagregowane dzienne dane dla cen mieszkań"
-                             ),
+                           p("Poniższy wykres przedstawia zagregowane dzienne dane dla cen mieszkań"),
                            plotOutput("plot"),
                            plotOutput("plot_holt"),
-                           verbatimTextOutput("holt")),
+                           'Optymalne wartości parametrów wygładzania, oparte na zminimalizowaniu błędów przewidywania o jeden stopień do przodu, wynoszą odpowiednio 0.9999188, 0 i 0 dla α, β i γ.',
+                           'Współczynniki są szacunkowymi wartościami poziomu, nachylenia i mnożników sezonowych od stycznia do grudnia, dostępnymi w ostatnim punkcie czasowym (t = n = 7).',
+                           htmlOutput("testHTML")),
+                           
                   tabPanel("Inna metoda", verbatimTextOutput("summary")),
                   tabPanel("Dane", dataTableOutput("dataTable"))
       )
